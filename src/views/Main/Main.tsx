ce67ec9638689a5965d9e8c7ee97a72ec6263db6
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2023-02-14 23:14:36 +0200

    add loader for main page

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,14 +1,35 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { FC } from "react";
 
 type tLanguage = "Rus" | "Ua";
 
+const buttonTextData = {
+  Rus: "Получить задание",
+  Ua: "Отримати завдання",
+};
+
 export const Main: FC = () => {
   const [lang, setLang] = useState<tLanguage>("Rus");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!loading) return;
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [loading]);
+
+  const handleFindTask = () => {
+    setLoading(true);
+  };
   return (
     <div className="Main">
-      <button className="Main-roll">
-        {lang === "Rus" ? "Получить задание" : "Отримати завдання"}
+      <div className="Main-logo">My Lady Slim</div>
+      <button className="Main-roll" onClick={handleFindTask} disabled={loading}>
+        {loading ? <span className="loader"></span> : buttonTextData[lang]}
       </button>
     </div>
   );
