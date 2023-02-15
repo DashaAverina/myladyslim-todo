cbaee665b1dc9000583439924593fe31305657ae
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2023-02-15 17:10:45 +0200

    update context

--- src/context/LangContext.tsx
+++ src/context/LangContext.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { createContext } from "vm";
 
 const LangContext = createContext({});
 
+type tLanguage = "Rus" | "Ua";
+
 export const LangContextProvider = ({ children }) => {
-  return <LangContext.Provider value={{}}> {children}</LangContext.Provider>;
+  const [lang, setLang] = useState<tLanguage>("Rus");
+  return (
+    <LangContext.Provider value={{ lang, setLang }}>
+      {children}
+    </LangContext.Provider>
+  );
 };
